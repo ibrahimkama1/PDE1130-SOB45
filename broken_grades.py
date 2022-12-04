2c7fed492f64633d680602ce1246da2889d2f8c7
unknown <ibrakamal321@gmail.com>
2022-12-04 17:14:05 +0400

    broken codes

--- broken_grades.py
+++ broken_grades.py
@@ -21,25 +21,26 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = int(input("Input exam grade two: "))
+exam_two = input("Input exam grade two: "))
 
-exam_three =int(input("Input exam grade three: "))
+exam_3 = str(input("Input exam grade three: "))
 
-grades = [exam_one,exam_two, exam_three]
+grades = [exam_one exam_two exam_three]
 sum = 0
-for grades in grades:
-    sum = sum + grades
+for grade in grade:
+  sum = sum + grade
 
-avg = sum / len(grades)
+avg = sum / len(grdes)
+
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90:
+elif avg >= 80 and avg < 90
     letter_grade = "B"
 elif avg > 69 and avg < 80:
-    letter_grade = "C"
+    letter_grade = "C'
 elif avg <= 69 and avg >= 65:
     letter_grade = "D"
-elif avg <=64 and avg >=60:
+elif:
     letter_grade = "F"
 
 for grade in grades:
@@ -49,7 +50,7 @@ for grade in grades:
 
     print("Grade: " + letter_grade)
 
-if letter-grade == "F":
-    'print' "Student is failing."
+if letter-grade is "F":
+    print "Student is failing."
 else:
-    'print' "Student is passing."
+    print "Student is passing."
